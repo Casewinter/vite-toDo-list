aad6a6c91828e2d3b55f87dec6984e1d1b78d9e6
Casewinter <viniciusbassilio@gmail.com>
2023-02-18 18:52:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vite-toDo-list
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-nkmtjj)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+
+let id = 0;
+const task = ref('');
+
+const toDos = ref([]);
+
+const newTask = () => {
+  toDos.value.push({
+    id: id++,
+    text: task.value,
+  });
+  task.value = '';
+};
+
+const deleteTask = (t) => {
+  toDos.value = toDos.value.filter((i) => i !== t);
+};
+</script>
+
+<template>
+  <input v-model="task" type="text" />
+  <li v-for="toDo in toDos" :key="toDo.id">
+    {{ toDo.text }}
+    <button @click="deleteTask(toDo)">Deletar</button>
+  </li>
+  <button @click="newTask">Adicionar tarefa</button>
+</template>
+
+<style scoped></style>
